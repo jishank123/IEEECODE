f8deb264a09c1de7d3adfbf2f022f461250beef3
jishank123 <techiesgajendra@gmail.com>
2025-06-07 14:15:36 +0530

    Add region field to Question model and update POST /questions APIs

--- models/Question.js
+++ models/Question.js
@@ -4,6 +4,7 @@ const QuestionSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   tags: [String],
+  region: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
